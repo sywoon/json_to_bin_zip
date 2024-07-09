89cd2dc7bfd919e484b5e59527f8e98d1be25e15
sywoon <sywoon@163.com>
2024-07-09 17:19:03 +0800

    上传原始json文件 新增getUnique

--- DbReaderSync.js
+++ DbReaderSync.js
@@ -42,8 +42,18 @@ class DbReaderSync {
                 console.log("load names time", Date.now() - time);
             }
 
-            let tableInfo = this.unzipOneTable("activity");
+            // let tableInfo = this.unzipOneTable("activity");
             let info = this.getDbById("activity", 1);
+            console.log("activity info", info);
+
+            let lines = this.readDbAll("activity");
+            console.log("lines all", Object.keys(lines));
+
+
+            {
+                let line = this.getUnique("beatGame_treasure", 1001, 3)
+                console.log("double_key beatGame_treasure", line)
+            }
         } catch (err) {
             console.error("binToJson error", err);
         }
@@ -62,20 +72,42 @@ class DbReaderSync {
         return tableInfo
     }
 
-    //这里的实现有问题 由于jszip库是异步的 导致获取数据时不能立刻返回
-    //解决：使用早期的2.6.1的同步版本 需要测试下性能
-    getDbById(name, id) {
+    getTableInfo(name) {
         let tableInfo = this.tableCache[name]
         if (!tableInfo) {
             tableInfo = this.unzipOneTable(name);
         }
+        return tableInfo;
+    }
 
+    checkTableInfo(name) {
+        this.getTableInfo(name);
+    }
+
+    //这里的实现有问题 由于jszip库是异步的 导致获取数据时不能立刻返回
+    //解决：使用早期的2.6.1的同步版本 需要测试下性能
+    getDbById(name, id) {
+        this.checkTableInfo(name);
         let line = this.readLineById(name, id);
-        console.log("line", line);
-
-        let lines = this.readDbAll(name);
-        console.log("lines all", Object.keys(lines));
         return line;
+    }
+
+    _getDoubleKeyId(name, ...args) {
+        let arrParams = Array.prototype.slice.call(args);
+        let key = arrParams.join("_");
+
+        let tableInfo = this.getTableInfo(name);
+        let id = tableInfo["double_keys"][key];
+        return id;
+    }
+
+    getUnique(name, ...args) {
+        let id = this._getDoubleKeyId(name, ...args);
+        if (!id) {
+            console.error("getUnique failed", name, args);
+            return null;
+        }
+        return this.getDbById(name, id);
     }
 
     readLineById(name, id) {
@@ -172,158 +204,6 @@ class DbReaderSync {
             console.error(`read db file failed:${path}`, err);
             return null;
         }
-    }
-
-    // headInfo: {version:20240301, file_num:2, table_num:999, tables:[{name,headOffset, bodyOffset}],
-    //  head_titles:["id", "name"], head_types:[0, 1]}
-    // bodyInfos: [[version:20240301, bodys:[]]]
-    saveJson(headInfo, bodyInfos) {
-        function replacer(key, value) {
-            if (typeof value === "function" || typeof value === "undefined") {
-                console.error("json.stringify error", key, value);
-                return null;
-            }
-            return value;
-        }
-
-        try {
-            fs.mkdirSync("./data2", { recursive: true });
-
-            let pathStrBuffer = `./data2/strings.json`;
-            fs.writeFileSync(pathStrBuffer, JSON.stringify(Object.values(this.strBuffer)));
-
-            let pathHead = `./data2/heads.json`;
-            fs.writeFileSync(pathHead, JSON.stringify(headInfo, replacer));
-
-            for (let i = 0; i < bodyInfos.length; i++) {
-                let pathBody = `./data2/data${i + 1}.json`;
-                try {
-                    let txt = JSON.stringify(bodyInfos[i], replacer);
-                    fs.writeFileSync(pathBody, txt);
-                } catch (err) {
-                    console.error("save body file failed", pathBody, err);
-                }
-            }
-        } catch (err) {
-            console.error("save bin file failed", err);
-        }
-    }
-
-    // {version:20240301, file_num:2, table_num:999, tables:[{name, headOffset, bodyOffset, dataFileIdx}],
-    //  head_titles:["id", "name"], head_types:[0, 1]}
-    readHead(byte) {
-        let headInfo = {};
-        headInfo["version"] = byte.readUint32();
-        headInfo["file_num"] = byte.readUint8();
-        headInfo["table_num"] = byte.readVarInt();
-
-        let tables = [];
-        for (let i = 0; i < headInfo["table_num"]; i++) {
-            let name = this.getStrFromByte(byte);
-            let headOffset = byte.readVarInt();
-            let bodyOffset = byte.readVarInt();
-            let dataFileIdx = byte.readUint8();
-            tables.push({ name: name, headOffset: headOffset, bodyOffset: bodyOffset, dataFileIdx: dataFileIdx });
-        }
-        headInfo["tables"] = tables;
-
-        //head data
-        let headTitles = [];
-        let headTypes = [];
-        for (let i = 0; i < headInfo["table_num"]; i++) {
-            let columnLen = byte.readUint8();
-            let headTitle = [];
-            for (let i = 0; i < columnLen; i++) {
-                headTitle.push(this.getStrFromByte(byte));
-            }
-            headTitles.push(headTitle);
-
-            let headType = [];
-            for (let i = 0; i < columnLen; i++) {
-                headType.push(byte.readUint8());
-            }
-            headTypes.push(headType);
-        }
-
-        headInfo["head_titles"] = headTitles;
-        headInfo["head_types"] = headTypes;
-        return headInfo;
-    }
-
-    // {version:20240301, bodys:[{name, doubleKey, values}]}
-    readBody(tableStart, dataFileIdx, byte, headInfo) {
-        let bodyInfo = {};
-        bodyInfo["version"] = byte.readUint32();
-
-        let bodys = [];
-        let tableNum = byte.readVarInt();
-
-        for (let i = 0; i < tableNum; i++) {
-            let tableIdx = i + tableStart;
-            let tableInfo = headInfo["tables"][tableIdx]; //{"name":"activity","headOffset":0,"bodyOffset":5,"dataFileIdx":0},
-            let head = headInfo["head_titles"][tableIdx]; //["id","name","fightItem","reward"
-            let headType = headInfo["head_types"][tableIdx]; //[0,1,0,0,0,
-
-            let body = this.readOneTableBody(byte, tableInfo, head, headType);
-            if (body == null) {
-                console.error("readOneTableBody failed", tableInfo, head, headType);
-                break;
-            }
-            bodys.push(body);
-        }
-        bodyInfo["bodys"] = bodys;
-        return bodyInfo;
-    }
-
-    // { name: name, doubleKey: doubleKey, values: values }
-    readOneTableBody(byte, tableInfo, head, headType) {
-        if (tableInfo.bodyOffset != byte.pos) {
-            console.error("bodyOffset error", name, tableInfo.bodyOffset, byte.pos);
-            return null;
-        }
-
-        let name = this.getStrFromByte(byte);
-        if (tableInfo.name != name) {
-            console.error("table name error", name, name);
-            return null;
-        }
-
-        let doubleKey = {};
-        let doubleKeysNum = byte.readVarInt();
-        for (let j = 0; j < doubleKeysNum; j++) {
-            let key = this.getStrFromByte(byte);
-            let id = byte.readVarInt();
-            doubleKey[key] = id;
-        }
-
-        let bodyLen = byte.readVarInt();
-        let values = [];
-        for (let j = 0; j < bodyLen; j++) {
-            let row = [];
-            for (let k = 0; k < head.length; k++) {
-                if (headType[k] == 0) {
-                    row.push(byte.readVarInt());
-                } else if (headType[k] == 1) {
-                    row.push(this.getStrFromByte(byte));
-                } else if (headType[k] == 2) {
-                    let txt = this.getStrFromByte(byte);
-                    row.push(JSON.parse(txt));
-                } else if (headType[k] == 3) {
-                    row.push(byte.readFloat32());
-                } else if (headType[k] == 4) {
-                    row.push(byte.readAny());
-                } else {
-                    console.error("readOneTableBody error", name, headType[k]);
-                }
-            }
-            values.push(row);
-        }
-
-        let body = { name: name, values: values };
-        if (doubleKeysNum > 0) {
-            body["doubleKey"] = doubleKey;
-        }
-        return body;
     }
 }
 
